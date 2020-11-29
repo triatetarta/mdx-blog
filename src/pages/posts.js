78613247025808cfb362935dc269c6ad1e430b45
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2020-11-29 10:30:27 +0000

    first commit

---
+++ src/pages/posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Hero from '../components/Hero'
+import { graphql } from 'gatsby'
+import Posts from '../components/Posts'
+// ...GatsbyImageSharpFluid
+const PostsPage = ({ data }) => {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default PostsPage
